923ece2a3e415346966bb33c39c1042db714bfcc
RonanzinDev <morellianogm@gmail.com>
2022-04-21 14:29:57 -0300

    Create the log file and add Date to then

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 namespace Alfasotf_Exercise;
 class Program
 {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
 
         try
@@ -12,27 +12,36 @@ class Program
             string path = Console.ReadLine() ?? "";
             var file = new StreamReader(path);
             string? user;
-            bool a = true;
-            while (a)
+            bool program = true;
+            while (program)
             {
                 while ((user = file.ReadLine()) != null)
                 {
-                    using (var client = new HttpClient())
+                    using (var Logfile = File.AppendText(currentDir + "/log.log"))
                     {
-                        client.BaseAddress = new Uri($"https://api.bitbucket.org/2.0/");
-                        client.DefaultRequestHeaders.Accept.Clear();
-                        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/jsons"));
-                        HttpResponseMessage response = client.GetAsync($"users/{user}").GetAwaiter().GetResult();
-                        var stream = response.Content.ReadAsStream();
-                        var reader = new StreamReader(stream);
-                        Console.WriteLine($"User: {user}");
-                        Console.WriteLine($"Url: https://api.bitbucket.org/2.0/{user}");
-                        Console.WriteLine($"Request output: \n{reader.ReadToEnd()}");
-                        Thread.Sleep(5000);
+                        using (var client = new HttpClient())
+                        {
+                            client.BaseAddress = new Uri($"https://api.bitbucket.org/2.0/");
+                            client.DefaultRequestHeaders.Accept.Clear();
+                            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/jsons"));
+                            HttpResponseMessage response = client.GetAsync($"users/{user}").GetAwaiter().GetResult();
+                            var stream = response.Content.ReadAsStream();
+                            var reader = new StreamReader(stream);
+                            var result = await reader.ReadToEndAsync();
+                            await Logfile.WriteLineAsync($"{result} - Date({DateTime.UtcNow})");
+                            Console.WriteLine(currentDir);
+                            Console.WriteLine($"User: {user}");
+                            Console.WriteLine($"Url: https://api.bitbucket.org/2.0/{user}");
+                            Console.WriteLine($"Request output: \n{result}");
+                            Logfile.Close();
+                            Thread.Sleep(5000);
+                        }
 
                     }
-
                 }
+                Console.WriteLine("Exiting...");
+                Thread.Sleep(5000);
+                program = false;
 
             }
         }
