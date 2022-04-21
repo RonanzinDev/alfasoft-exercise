190d5d2e094b96bcc9dff588943866e7ce1e7156
RonanzinDev <morellianogm@gmail.com>
2022-04-21 16:23:44 -0300

    Adding GetId function to get user id

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 ﻿using System.Net.Http.Headers;
+using System.Linq;
+using Newtonsoft.Json.Linq;
 namespace Alfasotf_Exercise;
 class Program
 {
     public static async Task Main(string[] args)
     {
-
         try
         {
             var currentDir = Directory.GetCurrentDirectory();
@@ -21,22 +22,24 @@ class Program
                     {
                         using (var client = new HttpClient())
                         {
+                            string id = GetId(user).Result;
                             client.BaseAddress = new Uri($"https://api.bitbucket.org/2.0/");
                             client.DefaultRequestHeaders.Accept.Clear();
                             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/jsons"));
-                            HttpResponseMessage response = client.GetAsync($"users/{user}").GetAwaiter().GetResult();
+                            HttpResponseMessage response = client.GetAsync($"users/{id}").GetAwaiter().GetResult();
                             var stream = response.Content.ReadAsStream();
                             var reader = new StreamReader(stream);
                             var result = await reader.ReadToEndAsync();
                             await Logfile.WriteLineAsync($"{result} - Date({DateTime.UtcNow})");
-                            Console.WriteLine(currentDir);
-                            Console.WriteLine($"User: {user}");
-                            Console.WriteLine($"Url: https://api.bitbucket.org/2.0/{user}");
-                            Console.WriteLine($"Request output: \n{result}");
+                            Console.WriteLine($"\nUser: {user}");
+                            Console.WriteLine($"\nUrl: https://api.bitbucket.org/2.0/user/{id}");
+                            Console.WriteLine($"\nRESPONSE: \n{result}");
+                            Console.WriteLine(id);
+                            Console.WriteLine("----------------------------------------------------------------------------------------------");
                             Logfile.Close();
                             Thread.Sleep(5000);
-                        }
 
+                        }
                     }
                 }
                 Console.WriteLine("Exiting...");
@@ -48,6 +51,32 @@ class Program
         catch (Exception e)
         {
             Console.WriteLine($"{e.Message.ToString()}");
+        }
+    }
+    public static async Task<string> GetId(string user)
+    {
+        string id;
+        using (var client = new HttpClient())
+        {
+            client.BaseAddress = new Uri($"https://api.bitbucket.org/2.0/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/jsons"));
+            HttpResponseMessage response = client.GetAsync($"workspaces/{user}?fields=uuid").GetAwaiter().GetResult();
+            var stream = response.Content.ReadAsStream();
+            var reader = new StreamReader(stream);
+            var result = await reader.ReadToEndAsync();
+            JObject o = JObject.Parse(result);
+            var value = (string)o.SelectToken("uuid");
+            id = value ?? "";
+
+        }
+        if (id != null)
+        {
+            return id;
+        }
+        else
+        {
+            return "Error find the id ";
         }
     }
 }
