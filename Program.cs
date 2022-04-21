a13afe29a828a170898a93e7e3629710f5497285
RonanzinDev <morellianogm@gmail.com>
2022-04-21 12:55:00 -0300

    Using try catch

--- Program.cs
+++ Program.cs
@@ -4,42 +4,41 @@ class Program
 {
     public static void Main(string[] args)
     {
-        var currentDir = Directory.GetCurrentDirectory();
-        Console.Write("Please enter the file path: ");
-        string? path = Console.ReadLine();
 
-        if (path == string.Empty)
+        try
         {
-            Console.WriteLine("Path can not be empty");
-        }
-        var file = new StreamReader(path);
-        string? user;
-        bool a = true;
-        while (a)
-        {
-            while ((user = file.ReadLine()) != null)
+            var currentDir = Directory.GetCurrentDirectory();
+            Console.Write("Please enter the file path: ");
+            string path = Console.ReadLine() ?? "";
+            var file = new StreamReader(path);
+            string? user;
+            bool a = true;
+            while (a)
             {
-                using (var client = new HttpClient())
+                while ((user = file.ReadLine()) != null)
                 {
-                    client.BaseAddress = new Uri($"https://api.bitbucket.org/2.0/");
-                    client.DefaultRequestHeaders.Accept.Clear();
-                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/jsons"));
-                    HttpResponseMessage response = client.GetAsync($"users/{user}").GetAwaiter().GetResult();
-                    var stream = response.Content.ReadAsStream();
-                    var reader = new StreamReader(stream);
-                    Console.WriteLine($"User: {user}");
-                    Console.WriteLine($"Url: https://api.bitbucket.org/2.0/{user}");
-                    Console.WriteLine($"Request output: \n{reader.ReadToEnd()}");
-                    Thread.Sleep(5000);
+                    using (var client = new HttpClient())
+                    {
+                        client.BaseAddress = new Uri($"https://api.bitbucket.org/2.0/");
+                        client.DefaultRequestHeaders.Accept.Clear();
+                        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/jsons"));
+                        HttpResponseMessage response = client.GetAsync($"users/{user}").GetAwaiter().GetResult();
+                        var stream = response.Content.ReadAsStream();
+                        var reader = new StreamReader(stream);
+                        Console.WriteLine($"User: {user}");
+                        Console.WriteLine($"Url: https://api.bitbucket.org/2.0/{user}");
+                        Console.WriteLine($"Request output: \n{reader.ReadToEnd()}");
+                        Thread.Sleep(5000);
 
-
+                    }
 
                 }
 
             }
-            Console.WriteLine("\n\nExiting...");
-            Thread.Sleep(5000);
-            a = false;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"{e.Message.ToString()}");
         }
     }
 }
